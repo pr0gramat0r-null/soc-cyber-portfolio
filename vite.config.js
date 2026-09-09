f51eb5c328af89cf8433f0bcae18acfe361d0557
pr0gramat0r-null <pr0gramat0r.null@gmail.com>
2026-09-09 15:38:28 +0200

    2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-/*import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { script } from "framer-motion/client";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: { host: "::", port: 5173 }
-});*/
+});
 
-export default
-  {
-    "scripts2": {
-      "test": "jest"
-    }
+import { scripts } from "/IT/soc-cyber-portfolio/package.json";
+
+({
+  "scripts": {
+    "test": "jest"
   }
+})
+
