af2761cee097c35e4ac0338955578d975cc21019
pr0gramat0r-null <pr0gramat0r.null@gmail.com>
2026-09-09 15:23:16 +0200

    Добавить скрипт тестирования в конфигурацию Vite и обновить package.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
+/*import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: { host: "::", port: 5173 }
-});
+});*/
+
+export default
+  {
+    "scripts": {
+      "test": "jest"
+    }
+  }
