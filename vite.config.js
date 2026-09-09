a732f1cdbdcc6138d0e3956ec03a2526c17706e4
pr0gramat0r-null <pr0gramat0r.null@gmail.com>
2026-09-09 15:42:48 +0200

    Обновить скрипт тестирования в конфигурации Vite с jest на vites

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { script } from "framer-motion/client";
+import { scripts } from "/IT/soc-cyber-portfolio/package.json";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: { host: "::", port: 5173 }
 });
-
-import { scripts } from "/IT/soc-cyber-portfolio/package.json";
-
 ({
   "scripts": {
-    "test": "jest"
+    "test": "vites"
   }
 })
 
